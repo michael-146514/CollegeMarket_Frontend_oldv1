436607fae7928f60be53c4e400cf364bb06c571b
Michael Santamaria <38706038+michael-146514@users.noreply.github.com>
2023-10-03 11:13:05 -0500

    Added CreateItem page, Added the file searchpage and itempage

--- src/components/ImageForm/ImageForm.jsx
+++ src/components/ImageForm/ImageForm.jsx
@@ -1,47 +1,86 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
-export default function ImageForm() {
-  // State variables for the form inputs
+export default function ProductForm() {
   const [title, setTitle] = useState("");
+  const [user, token] = useAuth();
   const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [condition, setCondition] = useState("");
+  const [zipcode, setZipcode] = useState("");
+  const [isActive, setIsActive] = useState(true);
+
   const [image, setImage] = useState(null);
 
-  // Handle form submission
+  const categories = [
+    "Textbooks",
+    "Electronics",
+    "Furniture",
+    "Clothing",
+    "Housing",
+    "Sports Equipment",
+    "Events and Tickets",
+    "Supplies",
+    "Jobs and Internships",
+    "Events and Parties",
+  ];
+
+  const conditions = [
+    "Brand New",
+    "Used but good",
+    "Okay",
+    "Not the best",
+    "Bad",
+  ];
+  const [category, setCategory] = useState("");
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleConditionChange = (event) => {
+    setCondition(event.target.value);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    // Append form data to the FormData object
     formData.append("title", title);
     formData.append("description", description);
-    formData.append("image_url", image);
+    formData.append("price", price);
+    formData.append("condition", condition);
+    formData.append("category", category);
+    formData.append("zipcode", zipcode);
+    formData.append("isActive", isActive);
     if (image !== null) {
-      formData.append("image_url", image);
+      formData.append("Images", image);
     }
+    formData.append("User", user);
 
     try {
-      // Send a POST request with the form data to the server
       const response = await axios.post(
-        "https://localhost:5001/api/image/",
+        "https://localhost:5001/api/products",
         formData,
         {
           headers: {
             "Content-Type": "multipart/form-data",
+            Authorization: "Bearer " + token,
           },
         }
       );
-      // Log the response data
-      console.log(response.data);
 
-      if (image !== null) {
-        formData.append("image_url", image);
-        console.log("null checked");
-      }
-    } catch (er) {
-      // Log any error response data
-      console.log(er.response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response ? error.response.data : error.message);
     }
   };
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <label>Title:</label>
@@ -52,17 +91,56 @@ export default function ImageForm() {
       />
       <label>Description:</label>
       <input
+        type="text"
         value={description}
         onChange={(event) => setDescription(event.target.value)}
       />
+      <label>Price:</label>
+      <input
+        type="number"
+        value={price}
+        onChange={(event) => setPrice(event.target.value)}
+      />
+      <label>Condition:</label>
+      <select value={condition} onChange={handleConditionChange}>
+        <option value="">Select a Condition</option>
+        {conditions.map((con, index) => (
+          <option key={index} value={con}>
+            {con}
+          </option>
+        ))}
+      </select>
 
-      {/* Image file input */}
+      <label>Category:</label>
+      <select value={category} onChange={handleCategoryChange}>
+        <option value="">Select a category</option>
+        {categories.map((cat, index) => (
+          <option key={index} value={cat}>
+            {cat}
+          </option>
+        ))}
+      </select>
+      <label>Zipcode:</label>
+      <input
+        type="text"
+        value={zipcode}
+        onChange={(event) => setZipcode(event.target.value)}
+      />
+      <label>Is Active:</label>
+      <input
+        type="checkbox"
+        checked={isActive}
+        onChange={(event) => setIsActive(event.target.checked)}
+      />
+
       <input
         type="file"
+        name="Images"
         accept="image/jpeg,image/png,image/gif"
-        onChange={(e) => setImage(e.target.files[0])}
+        multiple
+        onChange={handleImageChange}
       />
-      <button type="submit">Submit Photo!</button>
+      <button type="submit">Submit Product!</button>
     </form>
   );
 }
---
+++ src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,5 @@
+const SearchedPage = ({}) => {
+  return <div></div>;
+};
+
+export default SearchedPage;
